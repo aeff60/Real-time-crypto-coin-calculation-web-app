4adb9393d9efc596956851e7a3ea7ff9a745a873
Aef_SB <46419278+aeff60@users.noreply.github.com>
2022-03-11 13:52:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home():
     return render_template('index.html', coinname=data_api_json)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)    
     
     
