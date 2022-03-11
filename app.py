9cd2a805eae93a463a48bdff6206713ddafecaae
Sirasit0397 <ssbunklnag@gmail.com>
2022-03-11 12:54:01 +0700

    edit fetch the API and create a form

--- app.py
+++ app.py
@@ -12,23 +12,8 @@ def home():
     response = urlopen(url_api)
     
     data_api_json = json.loads(response.read())
-    data_USDT_lsit = []
 
-    for i in range(len(data_api_json)):
-        coinname_all = data_api_json[i]['symbol']
-        find_USDT = coinname_all.find("USDT")
-        if find_USDT != -1:
-            data_USDT_lsit.append(data_api_json[i])
-        else:
-            pass
-        
-    coinname_USDT_list = []
-    for j in range(len(data_USDT_lsit)):
-        coinname_USDT = data_USDT_lsit[j]['symbol']
-        coinname_USDT_list.append(coinname_USDT)
-    print(coinname_USDT_list)
-    print(type(coinname_USDT_list))
-    return render_template('index.html', coinname=coinname_USDT_list)
+    return render_template('index.html', coinname=data_api_json)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
