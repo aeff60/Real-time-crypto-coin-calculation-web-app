d8ea83dc3205dba8ed25ca17f791f71795967cb7
Sirasit0397 <ssbunklnag@gmail.com>
2022-03-11 11:56:10 +0700

    Initial a flask app project

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import *  
+import os
+
+app  = Flask(__name__)
+
+@app.route('/') 
+
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)    
